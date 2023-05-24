5089f9d24c84b80de11cb9a601fe898f678d72a6
root <mgiraudo@ies21.edu.ar>
2023-05-24 13:20:50 -0300

    jk

--- ClsManejoBD.cs
+++ ClsManejoBD.cs
@@ -4,31 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
 using System.Data.OleDb;
+using System.Windows.Forms;
 
 namespace pryPonssaNeptuno
 {
     internal class ClsManejoBD
     {
+        //OBJETOS para manipular la conexiòn y datos de una BD
+        //zona de declaraciones de objetos y variables
         OleDbConnection conn;
         OleDbCommand comm;
         OleDbDataReader rdr;
 
         string ProveedorAcceso = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =";
+        public string RutaDeBaseDatos = "-";
 
         public void ConectarBD()
         {
             try
             {
+                //crea el objeto en memoria (instanciar)
                 conn = new OleDbConnection();
+                //debo ingresar la cadena de conexiòn (proveedor de la base, ruta, nombre de archivo)
                 conn.ConnectionString = ProveedorAcceso + "NEPTUNO.accdb";
                 conn.Open();
                 
+                MessageBox.Show("Base de Datos abierta - con propiedades de la clase");
+
             }
-            catch (Exception exc)
+            catch (Exception fallo)
             {
-                
+                MessageBox.Show("Error: " + fallo.Message);
             }
+        }
+        public void ListarTablasDeLaBaseDeDatos()
+        {
+            DataTable tablas;
+            tablas = conn.GetSchema("Tables");
+
+            //https://social.msdn.microsoft.com/Forums/es-ES/8b06cfb9-ce9b-4ad4-a8d5-53f0f281f198/obtener-el-nombre-de-todas-las-tablas-existentes-en-una-base-de-datos-acces-en-c?forum=vcses
+
         }
     }
 }
